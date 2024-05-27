fae449372c28629c89caa20e5410d2a7baf53ae8
Artur Gaetov <agaetov@gmail.com>
2024-05-27 18:24:19 +0300

    hw#22.2 ex1

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'catalog',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -131,3 +132,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
---
+++ users/forms.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+from users.models import User
+
+
+class UserRegisterForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = ('email', 'password1', 'password2',)
+
+
+
