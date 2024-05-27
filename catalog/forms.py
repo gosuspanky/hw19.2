e4992273ffd85e0a5c4829da2700e2de41728a64
Artur Gaetov <agaetov@gmail.com>
2024-05-27 16:17:18 +0300

    hw#22.1 ex3

--- catalog/forms.py
+++ catalog/forms.py
@@ -8,11 +8,17 @@ class ProductForm(forms.ModelForm):
         model = Product
         fields = ('name', 'description', 'price_per_purchase', "category")
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
     def clean_name(self):
         cleaned_data = self.cleaned_data['name']
 
         forbidden_words_list = ["казино", "криптовалюта", "крипта", "биржа", "дешево", "бесплатно", "обман", "полиция",
-                           "радар"]
+                                "радар"]
 
         if cleaned_data.lower() in forbidden_words_list:
             raise forms.ValidationError('В названии применено некорректное слово')
